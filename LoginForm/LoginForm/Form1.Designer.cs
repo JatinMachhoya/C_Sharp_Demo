96c653f5dd94a32401155a95dc40b51c5bef84e1
JatinMachhoya <jatinmachhoya18@gmail.com>
2024-10-24 23:46:35 +0530

    dsv

---
+++ LoginForm/LoginForm/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtunm = new System.Windows.Forms.TextBox();
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtunm
+            // 
+            this.txtunm.Location = new System.Drawing.Point(208, 47);
+            this.txtunm.Name = "txtunm";
+            this.txtunm.Size = new System.Drawing.Size(100, 22);
+            this.txtunm.TabIndex = 1;
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Location = new System.Drawing.Point(208, 127);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.PasswordChar = '.';
+            this.txtpwd.Size = new System.Drawing.Size(100, 22);
+            this.txtpwd.TabIndex = 2;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(91, 202);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(89, 34);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(208, 202);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(84, 34);
+            this.Login.TabIndex = 3;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 331);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtunm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtunm;
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LoginForm/LoginForm/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if(txtunm.Text=="Jatin" && txtpwd.Text == "3424")
+            {
+                MessageBox.Show("Login is Done", "Login");
+                Form2 f2 = new Form2();
+                f2.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Enter Valid Username and Password","Error");    
+            }
+        }
+
+        void clear()
+        {
+            txtunm.Text = "";
+            txtpwd.Text = "";
+        }
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+    }
+}
